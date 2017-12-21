d700d46996309a5b6cfafa4ed71f22e0fb56112c
William <William@163.com>
2017-12-21 20:56:28 +0800

    swift初体验

---
+++ Swift初体验.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: A UIKit based Playground for presenting user interface
+
+//Swift中如何导入框架
+//#import<UIKit/UIKit.h>
+import UIKit
+//定义标识符
+//int a = 20;
+//Swift中定义标识符前必须告诉编译器是一个常量还是变量
+//let声明常量
+let a : Int = 20
+//var声明变量
+var b : Double = 1.44
+b = 2.44
+//语句结束
+//如果一行中只有一条语句可以不加分号，若一行中有多行语句必须写分号
+//打印内容
+//NSLog(@"%d", a);
+print(a)
+
+
+
